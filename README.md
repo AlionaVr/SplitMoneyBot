aa15fa237cf3592bcc64960edb052485343ff3cc
AlionaVr <Alonavershilo@yandex.by>
2025-10-23 16:11:07 +0300

    Integrate PostgreSQL database
    Configured PostgreSQL as the application's database with updated connection properties. Added repositories for managing `AppUser` and `Expense` entities. Refactored code to support database integration and updated README to reflect changes.

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## Technologies:
 - JAVA, Spring Boot,
-- PostgreSQL (to save user's info, spendings)
+- PostgreSQL (to save appUser's info, spendings)
 - Telegram Bot API
 
 ## TODO:
 1) User input their name, save it in DB
 2) User input spendings -> save it in DB 
 3) Connect to Telegram Bot API
-4) user can get their own spendings, but must not see spendings of other users.
+4) appUser can get their own spendings, but must not see spendings of other users.
--- src/main/java/org/splitmoneybot/config/BotConfig.java
+++ src/main/java/org/splitmoneybot/config/BotConfig.java
@@ -2,24 +2,20 @@ package org.splitmoneybot.config;
 
 import lombok.Data;
 import org.splitmoneybot.SplitMoneyBot;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
 @Data
-@PropertySource("application.properties")
 public class BotConfig {
 
     private final SplitMoneyBot tgBot;
-    @Value("${telegram.bot.token}")
-    private String TGTOKEN;
-    @Value("${telegram.bot.name}")
-    private String TGNAME;
 
+    @EventListener(ContextRefreshedEvent.class)
     public void init() throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
